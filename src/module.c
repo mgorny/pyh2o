f5a81e776f2ced98d54a6072938414e0643ce029
Michał Górny <mgorny@gentoo.org>
2012-06-09 11:04:15 +0200

    Support Py3 module initialization API.

--- src/module.c
+++ src/module.c
@@ -5,14 +5,29 @@
 
 #include <Python.h>
 
+#if PY_MAJOR_VERSION >= 3
+#	define PY3
+#endif
+
 #include "h2o.h"
+
+#ifdef PY3
+
+static PyModuleDef h2o_module = {
+	PyModuleDef_HEAD_INIT, /* m_base */
+
+	"h2o", /* m_name */
+	"IF97 water & steam properties using libh2o", /* m_doc */
+	-1, /* m_size */
+	NULL /* m_methods */
+};
+
+#else
 
 static PyMethodDef global_methods[] = {
 	{NULL}
 };
 
-#ifndef PyMODINIT_FUNC /* declarations for DLL import/export */
-#	define PyMODINIT_FUNC void
 #endif
 
 PyMODINIT_FUNC
@@ -23,9 +38,17 @@ inith2o()
 	if (PyType_Ready(&H2O_type) < 0)
 		return;
 
+#ifdef PY3
+	m = PyModule_Create(&h2o_module);
+#else
 	m = Py_InitModule3("h2o", global_methods,
 		"IF97 water & steam properties using libh2o");
+#endif
 
 	Py_INCREF(&H2O_type);
 	PyModule_AddObject(m, "H2O", (PyObject*) &H2O_type);
+
+#ifdef PY3
+	return m;
+#endif
 }
